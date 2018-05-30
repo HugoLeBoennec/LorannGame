66fdb743a19a574ab15bb836ad2067214855540c
HugoLeBoennec <hugo.leboennec@viacesi.fr>
2018-05-30 10:50:52 +0200

    add

--- Lorann/view/src/main/java/view/ViewFacade.java
+++ Lorann/view/src/main/java/view/ViewFacade.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
@@ -12,8 +13,8 @@ import showboard.*;
 /**
  * <h1>The Class ViewFacade provides a facade of the View component.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * @author Hugo Le Boënnec jhugo.leboennec@cesi.fr
+ * @version 1.1
  */
 public class ViewFacade implements Runnable{
 	
@@ -21,37 +22,40 @@ public class ViewFacade implements Runnable{
      * 
      * Instantiates a new view facade.
      */
-	int y = 0;
-	int x = 0;
-	static private int	WIDTH = 20;
-	static private int	HEIGHT = 12;
+	/** The width. */
+	static private int WIDTH = 20;
+	/** The height. */
+	static private int HEIGHT = 12;
+	
 	
     public ViewFacade() {
 		super();
-		m_cam = new Rectangle(0, 0, (640), (384));
+		
+		/** call separate tread **/
     	SwingUtilities.invokeLater(this);
     	
     }
     
     public void run() {
-    	
+    	/** Create **/
     	final BoardFrame window;
     	
     	window = new BoardFrame("Lorann Game");
-    	window.setDimension(new Dimension(640, 384));
-    	window.setDisplayFrame(m_cam);
-    	window.setSize(640, 384);
+    	window.setDimension(new Dimension(WIDTH * 32, HEIGHT * 32));
+    	window.setDisplayFrame(new Rectangle(0, 0, WIDTH * 32, HEIGHT * 32));
+    	window.setSize(WIDTH * 32, HEIGHT * 32);
     	
-    for( y = 0; y < HEIGHT; y++) {
+    for( int y = 0; y < HEIGHT; y++) {
     	
-    	for( x = 0; x < WIDTH; x++) {
+    	for( int x = 0; x < WIDTH; x++) {
     		
-    		window.addSquare(square, x, y);
+    		window.addSquare(, x, y);
     		
     	}
     	
     }
     }
+    
     
     
     
