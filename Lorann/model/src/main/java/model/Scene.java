8a525a555e593842121f07a6a11db9bcdc7516d0
rubisetcie <rubisetcie@10.192.131.72>
2018-05-31 10:50:20 +0200

    Ajout de la gestion du personnage

--- Lorann/model/src/main/java/model/Scene.java
+++ Lorann/model/src/main/java/model/Scene.java
@@ -92,26 +92,29 @@ public class Scene implements IScene {
 				
 				el = ElementDAO.getElementByPos(level, x, y);
 				
-				// On insère l'objet en fonction du type :
+				// Object insertion :
 				switch (el.getType())
 				{
 					case 'b' : obj = new Bulle(x, y, Sprite.SPRITE_BULLE, this);		frame.addSquare(obj, x, y); break;
 					case 'p' : obj = new Sortie(x, y, Sprite.SPRITE_PORTE, this);		frame.addSquare(obj, x, y); break;
-					case 'd' : /*obj = new Lorann(x, y, Sprite.SPRITE_PORTE, level);*/	frame.addSquare(new Mur(x, y, Sprite.SPRITE_MUR, this), x, y); break;
 					case 'r' : obj = new Mur(x, y, Sprite.SPRITE_MUR, this);			frame.addSquare(obj, x, y); break;
 					case 'h' : obj = new SolHorizontal(x, y, Sprite.SPRITE_SOLH, this);	frame.addSquare(obj, x, y); break;
 					case 'v' : obj = new SolVertical(x, y, Sprite.SPRITE_SOLV, this);	frame.addSquare(obj, x, y); break;
-					case 'n' : obj = new DemonNord(x, y, Sprite.SPRITE_DEMONN, this);	frame.addSquare(new Mur(x, y, Sprite.SPRITE_MUR, this), x, y); break;
-					case 'w' : obj = new DemonOuest(x, y, Sprite.SPRITE_DEMONW, this);	frame.addSquare(new Mur(x, y, Sprite.SPRITE_MUR, this), x, y); break;
-					case 'e' : obj = new DemonEst(x, y, Sprite.SPRITE_DEMONE, this);	frame.addSquare(new Mur(x, y, Sprite.SPRITE_MUR, this), x, y); break;
-					case 's' : obj = new DemonSud(x, y, Sprite.SPRITE_DEMONS, this);	frame.addSquare(new Mur(x, y, Sprite.SPRITE_MUR, this), x, y); break;
+					case 'n' : obj = new DemonNord(x, y, Sprite.SPRITE_DEMONN, this);	frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
+					case 'w' : obj = new DemonOuest(x, y, Sprite.SPRITE_DEMONW, this);	frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
+					case 'e' : obj = new DemonEst(x, y, Sprite.SPRITE_DEMONE, this);	frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
+					case 's' : obj = new DemonSud(x, y, Sprite.SPRITE_DEMONS, this);	frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
 					case 'o' : obj = new Bourse(x, y, Sprite.SPRITE_BOURSE, this);		frame.addSquare(obj, x, y); break;
+					case 'd' : this.character.setX(x); this.character.setY(y);			frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
 					default : frame.addSquare(new Vide(x, y, Sprite.SPRITE_VIDE, this), x, y); break;
 				}
 				
 				setObjectXY(obj, x, y);
 			}
 		}
+		
+		// Main character creation :
+		frame.addPawn(this.character);
 	}
 	
 	@Override
